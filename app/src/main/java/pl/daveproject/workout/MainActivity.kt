9c466f8484b09cc06c3d58aa0aef8f911ab71f91
Dave <maciak.dawid.96@gmail.com>
2021-03-03 15:33:08 +0100

    Create custom start button

--- app/src/main/java/pl/daveproject/workout/MainActivity.kt
+++ app/src/main/java/pl/daveproject/workout/MainActivity.kt
@@ -2,10 +2,17 @@ package pl.daveproject.workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<LinearLayout>(R.id.llStart)
+        startBtn.setOnClickListener { view ->
+            Toast.makeText(this, "Start exercises", Toast.LENGTH_SHORT).show()
+        }
     }
 }
