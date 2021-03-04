dd7d025c764387bae24d0f8d6ed8c08f6f12e05c
Dave <maciak.dawid.96@gmail.com>
2021-03-04 16:29:03 +0100

    create exercise timer

--- app/src/main/java/pl/daveproject/workout/ExerciseActivity.kt
+++ app/src/main/java/pl/daveproject/workout/ExerciseActivity.kt
@@ -1,10 +1,22 @@
 package pl.daveproject.workout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class ExerciseActivity : AppCompatActivity() {
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
@@ -12,11 +24,76 @@ class ExerciseActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar_exercise_activity)
         setSupportActionBar(toolbar)
         val actionbar = supportActionBar
-        if(actionbar != null) {
+        if (actionbar != null) {
             actionbar.setDisplayHomeAsUpEnabled(true)
         }
-        toolbar.setNavigationOnClickListener{ view ->
+        toolbar.setNavigationOnClickListener { view ->
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    override fun onDestroy() {
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+    }
+
+    private fun setRestProgressBar() {
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val tvTimer = findViewById<TextView>(R.id.tvTimer)
+        progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                progressBar.progress = 10 - restProgress
+                tvTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                val exerciseView = findViewById<LinearLayout>(R.id.llExerciseView)
+                exerciseView.visibility = View.VISIBLE
+
+                val restView = findViewById<LinearLayout>(R.id.llRestView)
+                restView.visibility = View.GONE
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setupRestView() {
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setExerciseProgressBar() {
+        val progressBar = findViewById<ProgressBar>(R.id.exerciseProgressBar)
+        val tvTimer = findViewById<TextView>(R.id.tvExerciseTimer)
+        progressBar.progress = exerciseProgress
+        exerciseTimer = object: CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                progressBar.progress = 30 - exerciseProgress
+                tvTimer.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Change exercise", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun setupExerciseView() {
+        if(exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+        setExerciseProgressBar()
     }
 }
