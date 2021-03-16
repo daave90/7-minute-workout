e21b5f4f16bd96de2a891c20123b88ff9c5d2b39
Dave <maciak.dawid.96@gmail.com>
2021-03-16 13:20:35 +0100

    Calculate BMI

--- app/src/main/java/pl/daveproject/workout/activity/BMIActivity.kt
+++ app/src/main/java/pl/daveproject/workout/activity/BMIActivity.kt
@@ -1,9 +1,15 @@
 package pl.daveproject.workout.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.Toolbar
 import pl.daveproject.workout.R
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +26,84 @@ class BMIActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener { view ->
             onBackPressed()
         }
+
+        val calculateBtn = findViewById<Button>(R.id.btnCalculateUnits)
+        calculateBtn.setOnClickListener {
+            val weightEt = findViewById<AppCompatEditText>(R.id.etMetricUnitWeight)
+            val heightEt = findViewById<AppCompatEditText>(R.id.etMetricUnitHeight)
+            if (validateMetricUnits()) {
+                val height = heightEt.text.toString().toFloat() / 100
+                val weight = weightEt.text.toString().toFloat()
+
+                val bmi = weight / (height * height)
+                displayBmiResult(bmi)
+            }
+        }
+    }
+
+    private fun validateMetricUnits(): Boolean {
+        var isValid = true
+        val weightValue = findViewById<AppCompatEditText>(R.id.etMetricUnitWeight)
+        val heightValue = findViewById<AppCompatEditText>(R.id.etMetricUnitHeight)
+
+        if (weightValue.text.toString().isEmpty() || heightValue.text.toString().isEmpty()) {
+            isValid = false
+        }
+        return isValid
+    }
+
+    private fun displayBmiResult(bmi: Float) {
+        val bmiLabel: String
+        val bmiDescription: String
+        if (bmi.compareTo(15f) <= 0) {
+            bmiLabel = "Very severely underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0
+        ) {
+            bmiLabel = "Severely underweight"
+            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0
+        ) {
+            bmiLabel = "Underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0
+        ) {
+            bmiLabel = "Normal"
+            bmiDescription = "Congratulations! You are in a good shape!"
+        } else if (java.lang.Float.compare(bmi, 25f) > 0 && java.lang.Float.compare(
+                bmi,
+                30f
+            ) <= 0
+        ) {
+            bmiLabel = "Overweight"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0
+        ) {
+            bmiLabel = "Obese Class | (Moderately obese)"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0
+        ) {
+            bmiLabel = "Obese Class || (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        } else {
+            bmiLabel = "Obese Class ||| (Very Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }
+        val tvYourBMI = findViewById<TextView>(R.id.tvYoursBMI)
+        val tvBMIValue = findViewById<TextView>(R.id.tvBMIValue)
+        val tvBMIType = findViewById<TextView>(R.id.tvBMIType)
+        val tvBMIDescription = findViewById<TextView>(R.id.tvBMIDescription)
+
+        tvYourBMI.visibility = View.VISIBLE
+        tvBMIValue.visibility = View.VISIBLE
+        tvBMIType.visibility = View.VISIBLE
+        tvBMIDescription.visibility = View.VISIBLE
+
+        // This is used to round the result value to 2 decimal values after "."
+        val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        tvBMIValue.text = bmiValue // Value is set to TextView
+        tvBMIType.text = bmiLabel // Label is set to TextView
+        tvBMIDescription.text = bmiDescription // Description is set to TextView
     }
 }
